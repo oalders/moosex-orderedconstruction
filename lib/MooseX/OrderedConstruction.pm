6dc290366dd8c6502eaf20b45ccf95be8269c51c
Florian Ragwitz <rafl@debian.org>
2018-06-12 17:51:26 +0000

    Explain how it works

--- lib/MooseX/OrderedConstruction.pm
+++ lib/MooseX/OrderedConstruction.pm
@@ -28,3 +28,25 @@ Moose::Exporter->setup_import_methods(%metaroles);
 1;
 
 # ABSTRACT: Initialize Moose attributes in a predictable order
+
+=head1 DESCRIPTION
+
+This module allows you to not worry about attribute initialisation order in
+your Moose constructors. It does so by considering all attributes to be lazy,
+and forcing generation of attribute values from defaults and builders that
+aren't explicitly lazy at the end of the construction stage.
+
+As such, it automates the following common pattern:
+
+  has foo => (
+    is => 'ro',
+    lazy => 1, # not necessary with this module
+    default => sub ($self) {
+      ... # something that might depend on other attributes
+    },
+  );
+
+  # also not necessary with this module
+  sub BUILD ($) { $self->foo } # force "foo" value to be built during construction
+
+=cut
